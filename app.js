83075d28da90ce764bc3e9363052f081ac3ebb75
sungminkim <0724ksm@naver.com>
2023-03-07 15:21:09 +0900

    made clock toggle button

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
-const secDiv = document.getElementById('sec');
-const minDiv = document.getElementById('min');
-const hourDiv = document.getElementById('hour');
-const clockP = document.getElementById('clock_p');
+const body = document.body;
+const clock_div = body.querySelector(".clock_div");
+const clock_p = body.querySelector(".clock_p");
+const clock_btn = body.querySelector(".clock_btn");
 
-setInterval(updateClock, 1000);
+clock_div.hidden = true;
 
-function updateClock(){
-	let date = new Date();
-	let sec = date.getSeconds() / 60;
-	let min = (date.getMinutes() + sec) / 60;
-	let hour = (date.getHours() + min) / 12;
-	secDiv.style.transform = "rotate(" + (sec * 360) + "deg)";
-	minDiv.style.transform = "rotate(" + (min * 360) + "deg)";
-	hourDiv.style.transform = "rotate(" + (hour * 360) + "deg)";
+function getTime() {
+	let date = new Date()
+	const hour = String(date.getHours()).padStart(2, "0");
+	const minute = String(date.getMinutes()).padStart(2, "0");
+	const second = String(date.getSeconds()).padStart(2, "0");
+	clock_p.innerHTML = `${hour}:${minute}:${second}`;
 }
 
-function clockPinnerText() {
-	let date = new Date();
-	let hour = String(date.getHours()).padStart(2, "0");
-	let minute = String(date.getMinutes()).padStart(2, "0");
-	let second = String(date.getSeconds()).padStart(2, "0")
-	clockP.innerHTML = `${hour}:${minute}:${second}`;
-}
+setInterval(getTime, 1000)
 
-clockPinnerText()
-
-setInterval(clockPinnerText, 1000)
-
-updateClock();
+clock_btn.addEventListener("click", () => {
+	if(clock_div.hidden == true) {
+		clock_div.hidden = false;
+	} else {
+		clock_div.hidden = true;
+	}
+})
