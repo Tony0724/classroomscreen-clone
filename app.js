dbf6c9526ab643d05f22c4f6f1d26bd86eb4ded1
sungminkim <0724ksm@naver.com>
2023-03-29 16:13:15 +0900

    made timer

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ const textarea = text_div.querySelector("textarea")
 const hide_btn = text_div.querySelector(".hide")
 const container = text_div.querySelector(".container");
 const show_btn = text_div.querySelector(".show");
+const time_div = body.querySelector(".time_div");
+const time_btn = body.querySelector(".time_btn");
+const start_btn = time_div.querySelector("#start_btn");
 
 clock_div.style.display = 'none';
 text_div.style.display = 'none';
+time_div.style.display = 'none';
 
 clock_btn.addEventListener("click", () => {
 	if(clock_div.style.display === 'none') {
@@ -28,6 +32,42 @@ text_btn.addEventListener("click", () => {
 		text_div.style.display = 'none';
 	}
 })
+
+time_btn.addEventListener("click",() => {
+	if(time_div.style.display === 'none') {
+		time_div.style.display = 'inline-block'
+	} else {
+		time_div.style.display = 'none';
+	}
+})
+
+const timer_p = time_div.querySelector('p');
+
+// console.log(timer_p)
+
+function countdown( elementName, minutes, seconds ) {
+    var element, endTime, hours, mins, msLeft, time;
+
+    function twoDigits( n ) {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    function updateTimer() {
+        msLeft = endTime - (+new Date);
+        if ( msLeft < 1000 ) {
+            timer_p.innerHTML = "Time is up!";
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            timer_p.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+    element = document.getElementById( elementName );
+    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
 
 function getTime() {
 	let date = new Date()
@@ -271,3 +311,114 @@ show_btn.addEventListener("click", () => {
 	container.hidden = false;
 	show_btn.hidden = true
 })
+
+let timerinitialX = 0;
+let timerinitialY = 0;
+
+let moveElementTimer = false;
+let TimerEvents = {
+	mouse: {
+		down: "mousedown",
+		move: "mousemove",
+		up: "mouseup",
+	},
+	touch: {
+		down: "touchstart",
+		move: "touchmove",
+		up: "touchend",
+	},
+};
+let TimerdeviceType = "";
+  //Detech touch device
+const timerisTouchDevice = () => {
+	try {
+	  //We try to create TouchEvent (it would fail for desktops and throw error)
+		document.createEvent("TouchEvent");
+		TimerdeviceType = "touch";
+		return true;
+	} catch (e) {
+		deviceType = "mouse";
+		return false;
+	}
+};
+timerisTouchDevice();
+  //Start (mouse down / touch start)
+	time_div.addEventListener(TextEvents[deviceType].down, (e) => {
+	e.preventDefault();
+	//initial x and y points
+	timerinitialX = !timerisTouchDevice() ? e.clientX : e.touches[0].clientX;
+	timerinitialY = !timerisTouchDevice() ? e.clientY : e.touches[0].clientY;
+	//Start movement
+	moveElementTimer = true;
+});
+  //Move
+time_div.addEventListener(TextEvents[deviceType].move, (e) => {
+	//if movement == true then set top and left to new X andY while removing any offset
+	if (moveElementTimer) {
+		e.preventDefault();
+		let newX = !timerisTouchDevice() ? e.clientX : e.touches[0].clientX;
+		let newY = !timerisTouchDevice() ? e.clientY : e.touches[0].clientY;
+		time_div.style.top =
+		time_div.offsetTop - (timerinitialY - newY) + "px";
+		time_div.style.left =
+		time_div.offsetLeft - (timerinitialX - newX) + "px";
+		timerinitialX = newX;
+		timerinitialY = newY;
+	}
+	const Bounding = time_div.getBoundingClientRect();
+	if (Bounding.top < -1) {
+		alert("The clock element is not on the viewport!");
+		time_div.style.top = 10 + 'px';
+	}
+	if (Bounding.left < -1) {
+		alert("The clock element is not on the viewport!");
+		time_div.style.left = 10 + 'px';
+	}
+	if (Bounding.bottom > (window.innerHeight || document.documentElement.clientHeight)) {
+		alert("The clock element is not on the viewport!");
+		time_div.style.top = window.innerHeight - 356 + 'px';
+	}
+	if (Bounding.right > (window.innerWidth || document.documentElement.clientWidth)) {
+		alert("The clock element is not on the viewport!");
+		time_div.style.left = window.innerWidth - 381 + 'px';
+	}
+});
+  //mouse up / touch end
+time_div.addEventListener(
+	TextEvents[deviceType].up,
+	(stopMovement = (e) => {
+		moveElementTimer = false;
+	})
+);
+time_div.addEventListener("mouseleave", stopMovement);
+time_div.addEventListener(TimerEvents[deviceType].up, (e) => {
+	moveElementTimer = false;
+});
+
+function handleTimerStart() {
+	let minute_value = null;
+	let second_value = null;
+	function handleMinuteTime() {
+		const prom = prompt("How long do you want to set the minute?", 10)
+		minute_value = prom;
+	}
+	function handleSecondTime() {
+		const prom = prompt("How long do you want to set the second", 30)
+		second_value = prom;
+	}
+	handleMinuteTime();
+	handleSecondTime();
+	if(isNaN(minute_value) || isNaN(second_value)) {
+		alert("Please write a number value!")
+	} else if(minute_value === "" && second_value === "") {
+		alert("Please write not a empty value!")
+	} else if(minute_value === "") {
+		countdown("time_div", 0, parseInt(second_value))
+	} else if(second_value === "") {
+		countdown("time_div", parseInt(minute_value), 0)
+	} else {
+		countdown("time_div", parseInt(minute_value), parseInt(second_value))
+	}
+}
+
+start_btn.addEventListener("click", handleTimerStart)
